6f09a39a4b1302fc5d1fb3982f8dbb453da46be6
ARTHUR KLUG JUNIOR <arthur.junior@alunos.sc.senac.br>
2024-04-08 19:30:53 -0300

    adicionado model tip

--- app/src/main/java/com/senac/calculadora/MainActivity.kt
+++ app/src/main/java/com/senac/calculadora/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.SeekBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -24,8 +25,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -54,35 +58,62 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp() {
-    Scaffold (
+    Scaffold(
         topBar = {
             MyTopBar()
         }
     ) {
-        Column (modifier = Modifier
-            .padding(it)
-            .padding(16.dp)) {
-            Row (
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .padding(16.dp)
+        ) {
+
+            val amount = remember {
+                mutableStateOf(0.0)
+            }
+
+            val custom = remember {
+                mutableStateOf(15)
+            }
+
+            val tip15 = remember {
+                mutableStateOf(0.0)
+            }
+
+            val totalTip15 = remember {
+                mutableStateOf(0.0)
+            }
+
+            val tipCustom = remember {
+                mutableStateOf(0.0)
+            }
+
+            val totalTipCustom = remember {
+                mutableStateOf(0.0)
+            }
+
+            Row(
                 verticalAlignment = Alignment.CenterVertically
-            ){
+            ) {
 
                 LabelComponent(labelResource = R.string.amount)
 
                 TextField(
-                    value = "",
-                    onValueChange =  {},
+                    value = amount.value.toString(),
+                    onValueChange = {amount.value = it.toDouble()},
                     modifier = Modifier.weight(3f)
                 )
 
             }
-            Row (
+            Row(
                 verticalAlignment = Alignment.CenterVertically
-            ){
+            ) {
 
                 LabelComponent(labelResource = R.string.custom)
 
                 Slider(
-                    value = 15f,
+                    value = custom.value.toFloat(),
                     onValueChange = {},
                     valueRange = 0f..30f,
                     modifier = Modifier
@@ -104,31 +135,58 @@ fun MyApp() {
                 )
 
                 Text(
-                    text = "18%",
+                    text = "${custom.value}",
                     textAlign = TextAlign.Center,
                     modifier = Modifier
                         .weight(1.5f)
                 )
             }
 
-            Row (
+            Row(
                 modifier = Modifier
                     .padding(top = 8.dp)
-            ){
+            ) {
 
                 LabelComponent(labelResource = R.string.tips)
 
                 Text(
-                    text = "R$ 0,00",
+                    text = "R$ ${tip15.value}",
                     modifier = Modifier
-                        .weight(1.5f),
+                        .weight(1.5f)
+                        .background(color = Color.LightGray),
                     textAlign = TextAlign.Center
                 )
 
                 Text(
-                    text = "R$ 0,00",
+                    text = "R$ ${tipCustom.value}",
                     modifier = Modifier
-                        .weight(1.5f),
+                        .weight(1.5f)
+                        .background(color = Color.LightGray),
+                    textAlign = TextAlign.Center
+                )
+
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 8.dp)
+            ) {
+
+                LabelComponent(labelResource = R.string.total)
+
+                Text(
+                    text = "R$ ${totalTip15.value}",
+                    modifier = Modifier
+                        .weight(1.5f)
+                        .background(color = Color.LightGray),
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    text = "R$ ${totalTipCustom.value}",
+                    modifier = Modifier
+                        .weight(1.5f)
+                        .background(color = Color.LightGray),
                     textAlign = TextAlign.Center
                 )
 
---
+++ app/src/main/java/com/senac/calculadora/model/Tip.kt
@@ -0,0 +1,17 @@
+package com.senac.calculadora.model
+
+data class Tip (
+
+    val amount: Float = 0.0f,
+    val custom: Int = 15
+) {
+
+    fun getTip15() = amount * 15 / 100
+
+    fun getTipCustom() = amount * custom / 100
+
+    fun getTotal15() = amount + (amount * getTip15())
+
+    fun getTotalCustom() = amount + (amount * getTipCustom())
+
+}
